36fb3f5ac778727e9da4f33997a2e82fbdf67c2d
wenyuan <xuwenyuan2016@163.com>
2017-12-21 15:39:20 +0800

    节点模块增加了fs和transport两个字段

--- parsers/nodes_parser.py
+++ parsers/nodes_parser.py
@@ -29,9 +29,11 @@ class NodesParser(object):
             node_ip = latest_data['ip'].split(':')[0]                                                   # optional data
             host = latest_data['host']
             single_data_node_status = {'node_id': node, 'node_ip': node_ip, 'host': host,
-                                       'indices': {'search': {}},
+                                       'indices': {'search': {},'docs':{},'segments':{},'refresh':{},'merges':{}},
                                        'os': {},
-                                       'jvm': {}
+                                       #'indices':{}
+                                       'jvm': {},
+                                       'diff': {}
                                        }
             query_total = data['indices']['search']['query_total']                                   # search_info,is history data in default
             query_time_in_millis = data['indices']['search']['query_time_in_millis']
@@ -68,6 +70,9 @@ class NodesParser(object):
             single_data_node_status['jvm']['gc']['collectors']['old']['current_collection_time_in_millis'] = \
                 latest_old_gc_time_in_millis - old_gc_time_in_millis
 
+
+
+            single_data_node_status['diff_gc_old_count'] = self.get_diff_value(data, latest_data, 'jvm.gc.collectors.old.collection_count')
             storage_nodes.append(single_data_node_status)
 
         esindex = "%s-%s" % (esindex_prefix, time.strftime('%Y.%m.%d'))
@@ -79,13 +84,26 @@ class NodesParser(object):
         })
         return values
 
+    def get_value_from_dic(self, valuedic, valuekey):
+        # 从这个嵌套的dic结构中获得指定key的值
+        # key只能是str类型
+        key = valuekey.split('.')
+        tmp = valuedic
+        #print tmp
+        for k in key:
+            tmp = tmp.get(k)
+        return tmp
+
+    def get_diff_value(self, oldnode, newnode, value_key):
+        return self.get_value_from_dic(newnode, value_key) - self.get_value_from_dic(oldnode, value_key)
+
     def parse2flat_data(self, node_data, latest_node_data, es_config_info):
         esindex_prefix = es_config_info['esindex_prefix']
         data_type = es_config_info['data_type']
         values = []
         timestamp = time.strftime('%Y-%m-%dT%H:%M:%S+08:00')
         final_data_template = {'@timestamp': timestamp, 'appname': 'monitor', 'type': data_type,    # normal data
-                               'nodes_status': {'indices': {'search': {}}, 'os': {}, 'jvm': {}}
+                               'nodes_status': {'indices': {'search': {},'docs':{},'segments':{},'refresh':{},'merges':{}}, 'os': {}, 'jvm': {}, 'diff': {}}
                                }
         for node in node_data.keys():
             data = node_data[node]
@@ -129,6 +147,41 @@ class NodesParser(object):
                 latest_old_gc_count - old_gc_count
             final_data['nodes_status']['jvm']['gc']['collectors']['old']['current_collection_time_in_millis'] = \
                 latest_old_gc_time_in_millis - old_gc_time_in_millis
+
+
+
+
+            final_data['nodes_status']['diff']['diff_gc_old_count'] = self.get_diff_value(data, latest_data, 'jvm.gc.collectors.old.collection_count')
+            final_data['nodes_status']['diff']['diff_gc_young_count'] = self.get_diff_value(data, latest_data, 'jvm.gc.collectors.young.collection_count')
+            final_data['nodes_status']['diff']['diff_fsio_count'] = self.get_diff_value(data, latest_data, 'fs.io_stats.total.operations')
+            final_data['nodes_status']['diff']['diff_fsio_write_count'] = self.get_diff_value(data,latest_data, 'fs.io_stats.total.write_operations')
+            final_data['nodes_status']['diff']['diff_fsio_write_kilobytes'] = self.get_diff_value(data, latest_data, 'fs.io_stats.total.write_kilobytes')
+            final_data['nodes_status']['diff']['diff_fsio_read_count'] = self.get_diff_value(data, latest_data, 'fs.io_stats.total.read_operations')
+            final_data['nodes_status']['diff']['diff_fsio_read_kilobytes'] = self.get_diff_value(data, latest_data, 'fs.io_stats.total.read_kilobytes')
+            final_data['nodes_status']['diff']['diff_indices_query_count'] = self.get_diff_value(data, latest_data, 'indices.search.query_total')
+            final_data['nodes_status']['diff']['diff_indices_query_time'] = self.get_diff_value(data, latest_data, 'indices.search.query_time_in_millis')
+            final_data['nodes_status']['diff']['diff_indices_fetch_count'] = self.get_diff_value(data, latest_data, 'indices.search.fetch_total')
+            final_data['nodes_status']['diff']['diff_indices_fetch_time'] = self.get_diff_value(data,latest_data, 'indices.search.fetch_time_in_millis')
+            final_data['nodes_status']['diff']['diff_indices_docs_count'] = self.get_diff_value(data,latest_data, 'indices.docs.count')
+            final_data['nodes_status']['diff']['diff_indices_docs_deleted'] = self.get_diff_value(data, latest_data, 'indices.docs.deleted')
+            final_data['nodes_status']['diff']['diff_indices_segments_count'] = self.get_diff_value(data, latest_data, 'indices.segments.count')
+            final_data['nodes_status']['diff']['diff_indices_refresh_count'] = self.get_diff_value(data, latest_data, 'indices.refresh.total')
+            final_data['nodes_status']['diff']['diff_indices_merges_count'] = self.get_diff_value(data, latest_data, 'indices.merges.total')
+            final_data['nodes_status']['diff']['diff_transport_rx_count'] = self.get_diff_value(data,latest_data, 'transport.rx_count')
+            final_data['nodes_status']['diff']['diff_transport_tx_count'] = self.get_diff_value(data,latest_data, 'transport.tx_count')
+            final_data['nodes_status']['diff']['diff_transport_rx_size_in_bytes'] = self.get_diff_value(data,latest_data, 'transport.rx_size_in_bytes')
+            final_data['nodes_status']['diff']['diff_transport_tx_size_in_bytes'] = self.get_diff_value(data,latest_data, 'transport.tx_size_in_bytes')
+
+            # fs
+            # indices  search.query_total, query_time, fetch (times , time )
+            #       docs
+            #       segements count,
+
+            # refresh ()
+            # merge
+            # transport rx, tx
+            #
+
 
             esindex = "%s-%s" % (esindex_prefix, time.strftime('%Y.%m.%d'))
             values.append({
