3ee47ccf7335185c6bfd2b9f130e04632e1fd1b3
xuwenyuan <wenyuan.xu@wxjuyun.com>
2017-12-22 18:00:23 +0800

    重构template

--- initjob/es_template.py
+++ initjob/es_template.py
@@ -7,9 +7,19 @@ flat_template是使用扁平化数据结构的template
 import requests
 import json
 
+template_name = 'cc-monitor-template'
+
 nodes_nested_template = {
-    "order": 0,
+    "order": 1,
     "template": "cc-monitor-*",
+    "settings": {
+        "index": {
+            "refresh_interval": "10s",
+            "number_of_shards": 5,
+            "number_of_replicas": 0,
+            "routing.allocation.total_shards_per_node": 1
+        }
+    },
     "mappings": {
         "nodes_status": {
             "properties": {
@@ -33,8 +43,16 @@ nodes_nested_template = {
 
 
 nodes_flat_template = {
-    "order": 0,
+    "order": 1,
     "template": "cc-monitor-*",
+    "settings": {
+        "index": {
+            "refresh_interval": "10s",
+            "number_of_shards": 5,
+            "number_of_replicas": 0,
+            "routing.allocation.total_shards_per_node": 1
+        }
+    },
     "mappings": {
         "nodes_status": {
             "properties": {
@@ -59,19 +77,19 @@ class EsTemplate(object):
             template = nodes_flat_template
         else:
             template = nodes_nested_template
-        url = "http://{0}:9200/_template/{1}".format(es_url, template['template'])
+        url = "http://{0}:9200/_template/{1}".format(es_url, template_name)
         r = requests.get(url)
         if r.status_code == 404:
             r = requests.put(url, data=json.dumps(template), headers={'content-type': 'application/json'})
             print(r.text)
             if r.status_code == 200:
-                print('put template %s success!' % template['template'])
+                print('put template %s success!' % template_name)
             else:
-                print('put template %s failed!' % template['template'])
+                print('put template %s failed!' % template_name)
         elif r.status_code == 200:
-            print('template %s exists!' % template['template'])
+            print('template %s exists!' % template_name)
         else:
-            print('get template %s error!' % template['template'])
+            print('get template %s error!' % template_name)
 
     def make_indices_template(self, es_url):
         pass
